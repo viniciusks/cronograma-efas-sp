12de2fa61e6a63227e1f4b3b0c7a13122e026aa1
Vinicius Kremer Santos <vinicius.kremer@gmail.com>
2021-09-02 21:16:06 -0300

    Adicionando links dos temas

--- src/app/components/links/links.component.ts
+++ src/app/components/links/links.component.ts
@@ -7,9 +7,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LinksComponent implements OnInit {
 
+  public infos: any;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.infos = [
+      {
+        title: "Temas Específicos",
+        link: "https://zoom.us/j/99151151015?pwd=SE9LWnBxckcrWm5DL0pjWGx2U0V3UT09"
+      },
+      {
+        title: "Temas Atuais",
+        link: "https://zoom.us/j/99151151015?pwd=SE9LWnBxckcrWm5DL0pjWGx2U0V3UT09"
+      },
+      {
+        title: "Tema Central",
+        link: "https://zoom.us/j/99151151015?pwd=SE9LWnBxckcrWm5DL0pjWGx2U0V3UT09"
+      }
+    ]
   }
 
 }
